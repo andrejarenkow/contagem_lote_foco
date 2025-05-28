c385f0f69de746eaeb45d27a7d7940574513f56d
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-05-28 13:47:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ def process_text(text, codigo_evento, codigo_fotografo):
 
 st.title("Contagem de fotos vendidas por lote")
 
-col1, col2 = st.columns([1,3])
+col1, col2 = st.columns([1,4])
 
 with col2:
     col3, col4 = st.columns([2,4])
